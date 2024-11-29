b0097005217ed45babb977590beee80735a7fd5d
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-11-29 14:42:28 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: '',
+            size: 150,
+            textSize: 50
+        }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        },
+        cicleText() {
+            return this.size > 300 ? 'Cutie ;)' : 'Hi!';
+        }
+    }
+}).mount('#app');
